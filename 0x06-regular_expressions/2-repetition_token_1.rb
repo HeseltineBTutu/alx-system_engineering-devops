6a91252d5e96f769dfaca50ea64d5b3b1ac62d65
HeseltineBTutu <heseltinetutu@gmail.com>
2024-02-20 04:35:13 -0800

    Add Ruby script to match 'hb{0,3}tn' pattern in input string

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,9 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_repetition(string)
-  regex = /hb?t?n/
-  match = string.scan(regex)
-  puts match.join
-end
-
-match_repetition(ARGV[0])
+puts ARGV[0].scan(/hb?t?n/).join
